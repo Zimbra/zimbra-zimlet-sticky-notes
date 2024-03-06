8ec20cf2e9edb20b42184a4f2008869ee98e6fbd
UmaShankar Avagadda <umagmrit@gmail.com>
2024-03-06 14:35:16 +0000

    ZBUG-3911:Create rpm/debian packages

--- scripts/postinst.sh
+++ scripts/postinst.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-echo "*** Configuring Zimbra-Zimlet-Sticky-Notes ***"
+
+packageName="Zimbra-Zimlet-Sticky-Notes"
+packageZipName="zimbra-zimlet-sticky-notes.zip"
+deployPath="/opt/zimbra/zimlets-network"
+
+echo "*** Configuring ${packageName} ***"
 su - zimbra -c "zmmailboxdctl status"
+
 if [ $? -ne 0 ]; then
    echo "*** Mailbox is not running... ***"
    echo "*** Follow the steps below as zimbra user ignore if installing through install.sh .. ***"
-   echo "*** Install the Zimbra-Zimlet-Sticky-Notes zimlet. ***"
-   echo "*** zmzimletctl -l deploy /opt/zimbra/zimlets-network/zimbra-zimlet-sticky-notes.zip ***"
+   echo "*** Install the ${packageName} zimlet. ***"
+   echo "*** zmzimletctl -l deploy ${deployPath}/${packageZipName} ***"
    echo "*** zmprov fc zimlet ***"
 else
-   echo "*** Deploying Zimbra-Zimlet-Sticky-Notes zimlet ***"
-   su - zimbra -c  "zmzimletctl -l deploy /opt/zimbra/zimlets-network/zimbra-zimlet-sticky-notes.zip"
+   echo "*** Deploying ${packageName} zimlet ***"
+   su - zimbra -c  "zmzimletctl -l deploy ${deployPath}/${packageZipName}"
    su - zimbra -c  "zmprov fc zimlet"
 fi
-echo "*** Zimbra-Zimlet-Sticky-Notes Installation Completed. ***"
+
+echo "*** ${packageName} Installation Completed. ***"
 echo "*** Restart the mailbox service as zimbra user. Run ***"
 echo "*** su - zimbra ***"
 echo "*** zmmailboxdctl restart ***"
